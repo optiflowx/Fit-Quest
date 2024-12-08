7e769e53dca92ce55953cf908ca6e0a6fc0e83bf
Prince B(Opti) <prbothojhb@student.wethinkcode.co.za>
2024-12-08 21:26:42 +0200

    refactor: Remove Leaderboard feature and clean up HomeScreen imports
    - Deleted LeaderboardPage implementation and its associated files.
    - Commented out references to the Leaderboard in HomeScreen to prevent unused imports.
    - Streamlined HomeScreen for better maintainability by removing unnecessary code.

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,11 +1,9 @@
 import 'package:fit_quest/presentation/dashboard/dashboard_page.dart';
 import 'package:fit_quest/presentation/dietary/dietry_page.dart';
 import 'package:fit_quest/presentation/exercise/exercise.dart';
-import 'package:fit_quest/presentation/leaderboard/leaderboard.dart';
+// import 'package:fit_quest/presentation/leaderboard/leaderboard.dart';
 import 'package:fit_quest/presentation/settings/settings.dart';
 import 'package:flutter/material.dart';
-
-import '../onboarding/welcome_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -32,11 +30,11 @@ class _MainScreenState extends State<HomeScreen> {
       'label': 'Exercises',
       'view': const ExercisePage(),
     },
-    {
-      'icon': Icons.leaderboard,
-      'label': 'Leaderboard',
-      'view': const LeaderboardPage(),
-    },
+    // {
+    //   'icon': Icons.leaderboard,
+    //   'label': 'Leaderboard',
+    //   'view': const LeaderboardPage(),
+    // },
     {
       'icon': Icons.fastfood,
       'label': 'Dietry Plan',
